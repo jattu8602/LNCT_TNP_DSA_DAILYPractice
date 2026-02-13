b91f561c0e96fe17c333136d2078d69f2a405a1a
jattu8602 <chaurasiyajatin68@gmail.com>
2026-02-13 23:46:52 +0530

    class Solution {     public int removeDuplicates(int[] nums) {         int slow = 0 ;         for (int fast = 0;fast<nums.length ;fast++){             if(nums[fast]!=nums[slow]){                 slow++;                 nums[slow] = nums[fast];
                }
    
            }
            return slow+1;
    
        }
    }

---
+++ JavaDSA/remove-duplicates-from-sorted-array.java
@@ -0,0 +1,18 @@
+// https://leetcode.com/problems/remove-duplicates-from-sorted-array/description/
+
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int slow = 0 ;
+        for (int fast = 0;fast<nums.length ;fast++){
+            if(nums[fast]!=nums[slow]){
+                slow++;
+                nums[slow] = nums[fast];
+
+            }
+
+        }
+        return slow+1;
+
+
+    }
+}
