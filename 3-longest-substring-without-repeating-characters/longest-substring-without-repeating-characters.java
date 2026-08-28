ada7dfcd5e3fcdc3cab2e9722df867fce3bd162b
Nitesh Chourasiya <140261740+jattu8602@users.noreply.github.com>
2026-08-28 06:43:36 +0530

    Time: 65 ms (30.37%) | Memory: 47.9 MB (20.85%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
@@ -3,18 +3,19 @@ class Solution {
         HashSet<Character> set  = new HashSet<>();
         int max = 0 ; 
         int left = 0 ; 
-        for(int right= 0 ;right<s.length();right++){
-            // first check if already present the current element in hashset and remove the element from last 
+
+        for(int right = 0 ; right<s.length();right++){
             while(set.contains(s.charAt(right))){
                 set.remove(s.charAt(left));
                 left++;
             }
-            // now add to set
             set.add(s.charAt(right));
-            // now max calculate
-            max = Math.max(max , right - left+1);
+
+            max = Math.max(max,right-left+1 );
+
         }
-        return max;
+        return max ; 
+
 
         
     }
